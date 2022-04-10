a71c6137ba588725a16ad96c4d48b097eae14f00
hamster <hamster@hamster.com>
2022-04-10 22:48:35 +0900

    login form to redirect homepage

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -4,16 +4,23 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
     @GetMapping("/login")
     public String login(){
 
         return "login_form";
 
+    }
+
+
+    @PostMapping("/login")
+    public String postlogin(){
+
+        return "redirect:";
     }
 
 
