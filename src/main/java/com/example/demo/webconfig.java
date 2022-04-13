7a811b4b5015957aac2087a881c9f8abdce419bf
오인석 <ssoboro1@gmail.com>
2022-04-13 15:40:03 +0900

    interceptor completed

---
+++ src/main/java/com/example/demo/webconfig.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import com.example.demo.interceptor.LogInterceptor;
+import com.example.demo.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webconfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/")
+                .excludePathPatterns("/css/**","/*/ico","/error");
+
+
+        registry.addInterceptor(new LoginInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/", "/members/add", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error"
+                );
+    }
+}
